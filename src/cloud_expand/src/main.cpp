f35433db4b46d8b23d67ef9b4525f5aacde6682e
Zili Wang <zili.wang@ygomi.com>
2018-03-22 11:21:08 +0800

    temp main.cpp

--- src/cloud_expand/src/main.cpp
+++ src/cloud_expand/src/main.cpp
@@ -17,8 +17,8 @@
 class Frame{
 public:
     int frame_id;
-    cv::Mat img;
-    std::vector<cv::Mat> descs;
+    std::string bag_name;
+    std::vector<cv::Mat> dess;
     std::vector<cv::KeyPoint> kps;
 };
 
@@ -32,24 +32,68 @@ public:
 
 class SystemFront{
 public:
-    bool init(){
-        
+    bool init(std::string bag_name_){
+        bag_name=bag_name_;
     }
-    bool processData(cv::Mat img, std::vector<Connection>& outConn){
+    bool processData(int frame_id, cv::Mat img, std::vector<Connection>& outConn){
         cv::Ptr<cv::ORB> extractor=cv::ORB::create();
         std::vector<cv::KeyPoint> kps;
         cv::Mat mask;
         cv::Mat descs;
         extractor->detectAndCompute(img, mask, kps, descs);
+        Frame frame;
+        frame.bag_name = bag_name;
+        frame.frame_id = frame_id;
         cv::Mat debug_img= img.clone();
         for (int i=0; i<(int)kps.size();i++){
-            cv::circle(debug_img, kps[i].pt, 1, cv::Scalar(255,0,0,255), 2);
+            frame.kps.push_back(kps[i]);
+            frame.descs.push_back(descs.row(i));
+            //cv::circle(debug_img, kps[i].pt, 1, cv::Scalar(255,0,0,255), 2);
         }
-        cv::imshow("chamo", debug_img);
-        cv::waitKey(-1);
+        
+        
+        //cv::imshow("chamo", debug_img);
+        //cv::waitKey(-1);
         
     }
 private:
+    void matchTwoFrame(cv::Mat descriptors1, cv::Mat descriptors2, std::vector<cv::DMatch> matches){
+        cv::BFMatcher matcher;
+        std::vector<cv::DMatch> matches;
+        matcher.match(descriptors1, descriptors2, matches);
+        std::sort(matches.begin(), matches.end());
+        std::vector<cv::DMatch> good_matches;
+        const int ptsPairs = std::min(1000, (int)(matches.size() * 0.4f));
+        for( int j = 0; j < ptsPairs; j++ )
+        {
+            good_matches.push_back(matches[j]);
+        }
+        
+        std::vector<cv::Point2f> points1(good_matches.size());
+        std::vector<cv::Point2f> points2(good_matches.size());
+        std::vector<int> points1_ind(good_matches.size());
+        std::vector<int> points2_ind(good_matches.size());
+        for(int i=0; i< good_matches.size();i++){
+            int kpInd1=good_matches[i].queryIdx;
+            int kpInd2=good_matches[i].trainIdx;
+            points1_ind[i]=kpInd1;
+            points2_ind[i]=kpInd2;
+            points1[i] =keypoints1[kpInd1].pt;
+            points2[i] =keypoints2[kpInd2].pt;
+        }
+        cv::Mat inliers;
+        findFundamentalMat(points1, points2, cv::FM_RANSAC, 1., 0.99, inliers);
+        std::vector<cv::DMatch> out_matches;
+        for( int j = 0; j < inliers.rows; j++ )
+        {
+            if(inliers.at<unsigned char>(j)==1){
+                out_matches.push_back(good_matches[j]);
+            }
+        }
+        return out_matches;
+    }
+    
+    std::string bag_name;
     std::list<Frame> frame_list;
 };
 
